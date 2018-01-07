524b87e2bdb966052957035d6c3732a2bb2c820e
khaled <khaledx5>
2018-01-07 15:03:08 +0200

    simple test cases by chai(assert/expect) library in mocha

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 module.exports = {
-   test:function () {
+   SayHello:function () {
         return 'hello';
 
+    },
+    GetSelection:function(){
+        var selection = { name:'Chai' , serve:'with Spices'};
+        return selection;
+    },
+    GetMyteam:function(){
+        var myTeam = [ 'Galal' , 'khaled' , 'ahmed' ,'Alzhary' ,'Wessam'];
+        return myTeam;
+    },
+    GetName:function(name){
+        this.name = name;
     }
 }
--- test/appTest.js
+++ test/appTest.js
@@ -1,8 +1,48 @@
 const assert = require('chai').assert;
+const expect = require('chai').expect;
 const app    = require('../app');
+//uses chai assert apis
+//['exists' , 'equal' , 'typeOf', 'isString' , 'isObject' , 'isArray']
+
+//uses chau expect apis
+//['expect']
 
 describe('App' , function(){
-    it('App should return Hello', function(){
-        assert.equal(app.test(),'hello');
+    describe('SayHello()' , function () {
+        it('SayHello should return exist value' , function(){
+            let result = app.SayHello();
+            assert.exists( result , 'result is neither `null` nor `undefined`' );
+         });
+        it('SayHello should return Hello', function(){
+            assert.equal(app.SayHello(),'hello');
+            //OR
+            expect(app.SayHello()).to.equal('hello'); 
+         });
+        // #1
+        it('SayHello should return type string' , function(){
+           let result = app.SayHello();
+           assert.typeOf( result , 'string' );
+    
+         });   
+        // #1 equal to #2
+        // #2
+        it('SayHello should return type string' , function(){
+            let result = app.SayHello();
+            assert.isString( result );
+     
+         });
+    });
+    describe('GetSelection()' , function () {
+        it('GetSelection should return object' , function(){
+            let result = app.GetSelection();
+            assert.isObject( result );
+         });
+    });
+    describe('GetMyteam()' , function () {
+        it('GetMyteam should return Array' , function(){
+            let result = app.GetMyteam();
+            assert.isArray( result );
+     
+         });
     });
 });
